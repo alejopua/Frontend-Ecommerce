acc48c6c61cc231bacb91375f29159836d03d244
alejopua <alejopua@gmail.com>
2023-05-09 15:09:55 -0500

    working on integration v2

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const menuDesktop = document.querySelector('.inactive');
+// variables menu desktop
 const menuEmail = document.querySelector('.navbar-email');
+const menuDesktop = document.querySelector('.desktop-menu');
+
+// variables menu mobile
+const burgerMenu = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+
 
 // SHOW AND HIDE MENU EMAIL
 menuEmail.addEventListener('click', () => {
     console.log('click');
     menuDesktop.classList.toggle('inactive');
 });
+
+// SHOW AND HIDE BURGUER MENU MOBILE
+burgerMenu.addEventListener('click', () => {
+    console.log('burguer');
+    menuMobile.classList.toggle('inactive');
+});
+
